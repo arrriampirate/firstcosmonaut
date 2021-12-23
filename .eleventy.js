80be4644143dcc401909e21f4eb21af2d541b4c4
andreykanov <andreykanov@ozon.ru>
2021-12-23 10:55:30 +0300

    pipi-pupu

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ const htmlmin = require("html-minifier")
 
 module.exports = function(eleventyConfig) {
 
+	// Copy the `img` and `css` folders to the output
+	eleventyConfig.addPassthroughCopy('img')
+	eleventyConfig.addPassthroughCopy('css')
+
 	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
 	eleventyConfig.addFilter('htmlDateString', (date) => {
 		return new Intl.DateTimeFormat('ru-RU', {
@@ -11,16 +15,16 @@ module.exports = function(eleventyConfig) {
 			day: 'numeric',
 			timeZone: 'UTC'
 		  }).format(new Date(date))
-	});
+	})
 
-	// Copy the `img` and `css` folders to the output
-	eleventyConfig.addPassthroughCopy('img')
-  	eleventyConfig.addPassthroughCopy('css')
+	eleventyConfig.addJavaScriptFunction('changeOrder', () => {
+
+	})
 
 	// Inline minified CSS
 	eleventyConfig.addFilter('cssmin', (code) => {
 		return new CleanCSS({}).minify(code).styles
-	});
+	})
 
 	// Minify HTML Output
 	eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
